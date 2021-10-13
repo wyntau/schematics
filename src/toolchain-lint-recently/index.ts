7cff57315911b82faa7056837c9d9934780d9e79
wyntau <github@wtliu.com>
2021-10-13 22:20:58 +0800

    refactor(toolchain-lint-recently): add lint-recently to package.json

--- src/toolchain-lint-recently/index.ts
+++ src/toolchain-lint-recently/index.ts
@@ -1,7 +1,32 @@
-import { mergeWith, Rule, url } from '@angular-devkit/schematics';
+import { chain, mergeWith, Rule, Tree, url } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import JSON5 from 'json5';
+import fs from 'fs';
+import path from 'path';
+import debugLib from 'debug';
+
+const debug = debugLib('@wyntau/schematics:toolchain-lint-recently');
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function toolchainLintRecently(_options: any): Rule {
-  return mergeWith(url('./files'));
+  return chain([
+    mergeWith(url('./files')),
+    function (tree: Tree) {
+      const packages = JSON5.parse(
+        fs.readFileSync(path.resolve(__dirname, '../shared/package.json'), { encoding: 'utf-8' })
+      );
+      const packageName = 'lint-recently';
+
+      debug(`parsed package.json: %O`, packages);
+
+      addPackageJsonDependency(tree, {
+        type: NodeDependencyType.Dev,
+        name: packageName,
+        version: packages[packageName],
+      });
+
+      return tree;
+    },
+  ]);
 }
