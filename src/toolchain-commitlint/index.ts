8849dd1eaf90dce01a1f0154195b34f26b0e9615
wyntau <github@wtliu.com>
2021-10-14 22:48:12 +0800

    refactor: use latest-versions

--- src/toolchain-commitlint/index.ts
+++ src/toolchain-commitlint/index.ts
@@ -1,11 +1,8 @@
 import { chain, mergeWith, Rule, Tree, url } from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
-import JSON5 from 'json5';
-import fs from 'fs';
-import path from 'path';
 import debugLib from 'debug';
-import { PackageJson } from 'type-fest';
 import { cyan } from 'colorette';
+import { latestVersions } from '../shared/latest-versions';
 
 const debug = debugLib('@wyntau/schematics:toolchain-commitlint');
 
@@ -15,15 +12,10 @@ export function toolchainCommitlint(_options: any): Rule {
   return chain([
     mergeWith(url('./files')),
     function (tree: Tree) {
-      const packages: PackageJson = JSON5.parse(
-        fs.readFileSync(path.resolve(__dirname, '../shared/package.json'), { encoding: 'utf-8' })
-      );
-      debug(`parsed package.json: %O`, packages);
-
       const packageNames = ['@commitlint/cli', '@commitlint/config-conventional'];
 
       packageNames.forEach((packageName) => {
-        const packageVersion = packages.dependencies![packageName];
+        const packageVersion = latestVersions[packageName];
 
         debug(`get ${cyan(packageName)} version: %s`, cyan(packageVersion));
 
--- src/toolchain-lint-recently/index.ts
+++ src/toolchain-lint-recently/index.ts
@@ -1,11 +1,8 @@
 import { chain, mergeWith, Rule, Tree, url } from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
-import JSON5 from 'json5';
-import fs from 'fs';
-import path from 'path';
 import debugLib from 'debug';
-import { PackageJson } from 'type-fest';
 import { cyan } from 'colorette';
+import { latestVersions } from '../shared/latest-versions';
 
 const debug = debugLib('@wyntau/schematics:toolchain-lint-recently');
 
@@ -15,13 +12,8 @@ export function toolchainLintRecently(_options: any): Rule {
   return chain([
     mergeWith(url('./files')),
     function (tree: Tree) {
-      const packages: PackageJson = JSON5.parse(
-        fs.readFileSync(path.resolve(__dirname, '../shared/package.json'), { encoding: 'utf-8' })
-      );
-      debug(`parsed package.json: %O`, packages);
-
       const packageName = 'lint-recently';
-      const packageVersion = packages.dependencies![packageName];
+      const packageVersion = latestVersions[packageName];
       debug(`get ${cyan(packageName)} version: %s`, cyan(packageVersion));
 
       addPackageJsonDependency(tree, {
