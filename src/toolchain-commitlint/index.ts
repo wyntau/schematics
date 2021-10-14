658288c70fddc3769e540ee8cc652670c6e81416
wyntau <wyntau@outlook.com>
2021-10-14 23:30:50 +0800

    refactor(commitlint): add schema and addTask to husky

--- src/toolchain-commitlint/index.ts
+++ src/toolchain-commitlint/index.ts
@@ -1,14 +1,18 @@
-import { chain, mergeWith, Rule, Tree, url } from '@angular-devkit/schematics';
+import { chain, mergeWith, noop, Rule, Tree, url } from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
 import debugLib from 'debug';
 import { cyan } from 'colorette';
 import { latestVersions } from '../shared/latest-versions';
+import { IToolchainCommitlintOptions } from './schema';
+import { addTask } from '../toolchain-husky/utility';
+import { camelCasedOptions } from '../shared';
 
 const debug = debugLib('@wyntau/schematics:toolchain-commitlint');
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function toolchainCommitlint(_options: any): Rule {
+export function toolchainCommitlint(_options: IToolchainCommitlintOptions): Rule {
+  const options = camelCasedOptions(_options);
   return chain([
     mergeWith(url('./files')),
     function (tree: Tree) {
@@ -28,5 +32,6 @@ export function toolchainCommitlint(_options: any): Rule {
 
       return tree;
     },
+    options.toolchainHusky ? addTask('commit-msg', 'npx commitlint') : noop(),
   ]);
 }
