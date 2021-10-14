580765a473cce236ce08725330d6df56eeb5a45d
wyntau <github@wtliu.com>
2021-10-14 23:15:17 +0800

    refactor(husky): update husky related

--- src/toolchain-husky/index.ts
+++ src/toolchain-husky/index.ts
@@ -1,13 +1,8 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { camelCasedOptions } from '../shared';
+import { Rule, url, mergeWith } from '@angular-devkit/schematics';
 import { IToolchainHuskySchema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function toolchainHusky(_options: IToolchainHuskySchema): Rule {
-  const options = camelCasedOptions(_options);
-  return (tree: Tree, _context: SchematicContext) => {
-    console.log(options);
-    return tree;
-  };
+  return mergeWith(url('./files'));
 }
