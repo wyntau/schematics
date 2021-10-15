8130ce89516e1611cbafaea80d93c5a11db3446e
wyntau <github@wtliu.com>
2021-10-15 09:51:13 +0800

    feat(toolchain-lint-staged): add lint-staged

--- src/toolchain-lint-staged/index.ts
+++ src/toolchain-lint-staged/index.ts
@@ -1,9 +1,37 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, mergeWith, noop, Rule, Tree, url } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import debugLib from 'debug';
+import { cyan } from 'colorette';
+import { latestVersions } from '../shared/latest-versions';
+import { IToolchainLintStagedSchema } from './schema';
+import { addTask } from '../toolchain-husky/utility';
+import { camelCasedOptions } from '../shared';
+
+const debug = debugLib('@wyntau/schematics:toolchain-lint-staged');
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function toolchainLintStaged(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+export function toolchainCommitlint(_options: IToolchainLintStagedSchema): Rule {
+  const options = camelCasedOptions(_options);
+  return chain([
+    mergeWith(url('./files')),
+    function (tree: Tree) {
+      const packageNames = ['lint-staged'];
+
+      packageNames.forEach((packageName) => {
+        const packageVersion = latestVersions[packageName];
+
+        debug(`get ${cyan(packageName)} version: %s`, cyan(packageVersion));
+
+        addPackageJsonDependency(tree, {
+          type: NodeDependencyType.Dev,
+          name: packageName,
+          version: packageVersion,
+        });
+      });
+
+      return tree;
+    },
+    options.toolchainHusky ? addTask('commit-msg', 'npx lint-staged') : noop(),
+  ]);
 }
