06889b6153a2e18d572c5cb50aafc1d9fc2d6887
wyntau <wyntau@outlook.com>
2021-10-16 20:02:36 +0800

    refactor(run-task): add check for run function

--- src/run-task/index.ts
+++ src/run-task/index.ts
@@ -1,21 +1,23 @@
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { noop, Rule, Tree } from '@angular-devkit/schematics';
 import { IRunTaskOptions } from './schema';
 import { Observable } from 'rxjs';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function runTask(_options: IRunTaskOptions): Rule {
-  return (tree: Tree) => {
-    return new Observable<Tree>((subscriber) => {
-      _options.run().then(
-        () => {
-          subscriber.next(tree);
-          subscriber.complete();
-        },
-        (error) => {
-          subscriber.error(error);
-        }
-      );
-    }) as any;
-  };
+  return _options.run && typeof _options.run === 'function'
+    ? (tree: Tree) => {
+        return new Observable<Tree>((subscriber) => {
+          _options.run().then(
+            () => {
+              subscriber.next(tree);
+              subscriber.complete();
+            },
+            (error) => {
+              subscriber.error(error);
+            }
+          );
+        }) as any;
+      }
+    : noop();
 }
--- src/run-task/index_spec.ts
+++ src/run-task/index_spec.ts
@@ -4,10 +4,10 @@ import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
-describe('run-command', () => {
+describe('run-task', () => {
   it('works', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('run-command', {}, Tree.empty()).toPromise();
+    const tree = await runner.runSchematicAsync('run-task', {}, Tree.empty()).toPromise();
 
     expect(tree.files).toEqual([]);
   });
