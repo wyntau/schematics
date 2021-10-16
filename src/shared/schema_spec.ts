d7af940508f706393ab7cc732c3d46c8babd72fc
wyntau <wyntau@outlook.com>
2021-10-16 12:55:24 +0800

    refactor(shared/schema): update test

--- src/shared/schema_spec.ts
+++ src/shared/schema_spec.ts
@@ -1,4 +1,4 @@
-import { camelCasedOptions, kebabCasedOptions } from './schema';
+import { camelCasedOptions, kebabCasedOptions, prefixedOptions, unprefixedOptions } from './schema';
 
 describe('schema utility functions', () => {
   it('camelCasedOptions', () => {
@@ -17,5 +17,35 @@ describe('schema utility functions', () => {
     };
 
     expect(kebabCasedOptions(options, 'test')).toEqual({ 'toolchain-eslint': true, 'toolchain-commitlint': true });
+  });
+
+  it('prefixedOptions', () => {
+    const options = {
+      foo: true,
+      bar: false,
+      baz: 'baz',
+    };
+
+    expect(prefixedOptions<typeof options, 'foo' | 'bar', 'prefix'>(options, ['foo', 'bar'], 'prefix', 'test')).toEqual(
+      {
+        'prefix-foo': true,
+        'prefix-bar': false,
+        baz: 'baz',
+      }
+    );
+  });
+
+  it('unprefixedOptions', () => {
+    const options = {
+      'prefix-foo': true,
+      'prefix-bar': false,
+      baz: 'baz',
+    };
+
+    expect(unprefixedOptions<typeof options, 'prefix'>(options, 'prefix', 'test')).toEqual({
+      foo: true,
+      bar: false,
+      baz: 'baz',
+    });
   });
 });
