0c7067961d09626bb2f3013096e78a9ae34f5ebb
wyntau <github@wtliu.com>
2021-10-16 14:29:24 +0800

    refactor(shared/schema): unprefixedOptions support multi prefix

--- src/shared/schema_spec.ts
+++ src/shared/schema_spec.ts
@@ -54,6 +54,16 @@ describe('schema utility functions', () => {
     });
   });
 
+  it('unprefixedOptions with multi prefix', () => {
+    const options = {
+      'foo-foo': true,
+      'bar-bar': false,
+      baz: 'baz',
+    };
+
+    expect(unprefixedOptions(options, ['foo', 'bar'])).toEqual({ foo: true, bar: false, baz: 'baz' });
+  });
+
   it('normalizeOptions', () => {
     const options = {
       fooBar: true,
