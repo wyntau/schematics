ac129454a0fb7f32d5d1e4543408b9b7b7a836a9
wyntau <github@wtliu.com>
2021-10-16 09:46:14 +0800

    feat(toolchain-yarn): add yarn support

--- src/toolchain-yarn/index.ts
+++ src/toolchain-yarn/index.ts
@@ -1,9 +1,8 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, contentTemplate, mergeWith, Rule, url } from '@angular-devkit/schematics';
+import { IToolchainYarnOptions } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function toolchainYarn(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+export function toolchainYarn(_options: IToolchainYarnOptions): Rule {
+  return mergeWith(apply(url('./files'), [contentTemplate(_options)]));
 }
