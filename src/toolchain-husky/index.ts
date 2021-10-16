1a42db8529cc0870b21d471f46846492e7bde661
wyntau <wyntau@outlook.com>
2021-10-16 09:31:40 +0800

    refactor(toolchain-husky): 使用封装方法安装依赖

--- src/toolchain-husky/index.ts
+++ src/toolchain-husky/index.ts
@@ -1,28 +1,8 @@
-import { Rule, url, mergeWith, chain, Tree } from '@angular-devkit/schematics';
-import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
-import { latestVersions } from '../shared/latest-versions';
-import debugLib from 'debug';
-import { cyan } from 'colorette';
-
-const debug = debugLib('@wyntau/schematics:toolchain-husky');
+import { Rule, url, mergeWith, chain } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependencyType } from '../shared/rules/dependencies';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function toolchainHusky(): Rule {
-  return chain([
-    mergeWith(url('./files')),
-    function (tree: Tree) {
-      const packageName = 'husky';
-      const packageVersion = latestVersions[packageName];
-
-      debug(`get ${cyan(packageName)} version: %s`, cyan(packageVersion));
-
-      addPackageJsonDependency(tree, {
-        type: NodeDependencyType.Dev,
-        name: packageName,
-        version: packageVersion,
-      });
-      return tree;
-    },
-  ]);
+  return chain([mergeWith(url('./files')), addPackageJsonDependency(['husky'], NodeDependencyType.Dev)]);
 }
