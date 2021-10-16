a1d8331d5914d9780f2864479c4b98fedde67501
wyntau <github@wtliu.com>
2021-10-16 21:07:22 +0800

    feat(shared/rules/files): add mergeWithIfNotExist

---
+++ src/shared/rules/files.ts
@@ -0,0 +1,21 @@
+import { apply, forEach, MergeStrategy, mergeWith, Rule, Source, Tree } from '@angular-devkit/schematics';
+import debugLib from 'debug';
+
+const debug = debugLib('@wyntau/schematics:rules/files');
+
+export function mergeWithIfNotExist(source: Source, strategy?: MergeStrategy): Rule {
+  return function (tree: Tree) {
+    return mergeWith(
+      apply(source, [
+        forEach((fileEntry) => {
+          if (tree.exists(fileEntry.path)) {
+            debug('file exists, skip %s', fileEntry.path);
+            return null;
+          }
+          return fileEntry;
+        }),
+      ]),
+      strategy
+    );
+  };
+}
