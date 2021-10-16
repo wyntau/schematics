d4d661468adf51fbabcf94b18eab0cf1b6e8661d
wyntau <wyntau@outlook.com>
2021-10-16 10:01:36 +0800

    feat(toolchain-typescript): add toolchain-typescript

--- src/toolchain-typescript/index.ts
+++ src/toolchain-typescript/index.ts
@@ -1,9 +1,11 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, Rule } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependencyType } from '../shared/rules/dependencies';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function toolchainTypescript(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+  return chain([
+    addPackageJsonDependency(['typescript'], NodeDependencyType.Dev),
+    addPackageJsonDependency(['tslib'], NodeDependencyType.Default),
+  ]);
 }
