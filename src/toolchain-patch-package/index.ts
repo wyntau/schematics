9be8dbf1b157d404d8b2fc477cbfca2968c8489d
wyntau <github@wtliu.com>
2021-10-16 10:05:33 +0800

    feat(toolchain-patch-package): add toolchain patch-package

--- src/toolchain-patch-package/index.ts
+++ src/toolchain-patch-package/index.ts
@@ -1,9 +1,19 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, Rule, Tree } from '@angular-devkit/schematics';
+import { JSONFile } from '@schematics/angular/utility/json-file';
+import { addPackageJsonDependency, NodeDependencyType } from '../shared/rules/dependencies';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function toolchainPatchPackage(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+  return chain([
+    addPackageJsonDependency(['patch-package', 'postinstall-postinstall'], NodeDependencyType.Dev),
+    function (tree: Tree) {
+      const packageJson = new JSONFile(tree, 'package.json');
+
+      packageJson.modify(['scripts', 'postinstall'], 'patch-package');
+      packageJson.modify(['scripts', 'patch-package'], "patch-package --exclude 'the-file-not-exist'");
+
+      return tree;
+    },
+  ]);
 }
