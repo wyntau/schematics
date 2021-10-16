d375a5da0e347ac1f6dba61ca51e9d932ba32000
wyntau <wyntau@outlook.com>
2021-10-16 20:20:46 +0800

    refactor(run-task): 去掉不必要的 rxjs

--- src/run-task/index.ts
+++ src/run-task/index.ts
@@ -1,23 +1,12 @@
-import { noop, Rule, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { IRunTaskOptions } from './schema';
-import { Observable } from 'rxjs';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function runTask(_options: IRunTaskOptions): Rule {
-  return _options.run && typeof _options.run === 'function'
-    ? (tree: Tree) => {
-        return new Observable<Tree>((subscriber) => {
-          _options.run().then(
-            () => {
-              subscriber.next(tree);
-              subscriber.complete();
-            },
-            (error) => {
-              subscriber.error(error);
-            }
-          );
-        }) as any;
-      }
-    : noop();
+  return async (tree: Tree, context: SchematicContext): Promise<void> => {
+    try {
+      _options.run && _options.run(tree, context);
+    }catch(e){} // eslint-disable-line
+  };
 }
