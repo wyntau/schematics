6bba356e9adac9e217bf64a69629fa40ef3e704a
wyntau <github@wtliu.com>
2021-10-16 22:38:11 +0800

    refactor(task): normalize task

--- src/task/index_spec.ts
+++ src/task/index_spec.ts
@@ -4,10 +4,10 @@ import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
-describe('run-task', () => {
+describe('task', () => {
   it('works', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('run-task', {}, Tree.empty()).toPromise();
+    const tree = await runner.runSchematicAsync('task', () => true, Tree.empty()).toPromise();
 
     expect(tree.files).toEqual([]);
   });
