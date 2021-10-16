31ea73cb007a2f89a002a1e8b7e287c11b2d5443
wyntau <wyntau@outlook.com>
2021-10-16 23:18:59 +0800

    docs: add comment

--- src/task/index.ts
+++ src/task/index.ts
@@ -2,6 +2,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
 import { ITaskOptions, TaskFn } from './schema';
 
+//
+// see
+
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function runTask(taskFn: ITaskOptions): Rule {
@@ -12,6 +15,10 @@ export function runTask(taskFn: ITaskOptions): Rule {
   };
 }
 
+/**
+ * add custom task like `yarn install`
+ * @see https://github.com/angular/angular-cli/issues/12678#issuecomment-489175547
+ */
 export function addTask(taskFn: TaskFn): Rule {
   return function (tree, context) {
     context.addTask(new RunSchematicTask<TaskFn>('task', taskFn));
