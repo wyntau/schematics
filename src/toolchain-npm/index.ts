77dbb99b2c9adfa139a5ab1517f2a5ba9f6191de
wyntau <github@wtliu.com>
2021-10-16 18:40:32 +0800

    refactor(toolchain-npm): add debug log

--- src/toolchain-npm/index.ts
+++ src/toolchain-npm/index.ts
@@ -13,6 +13,9 @@ import {
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { camelCasedOptions } from '../shared/schema';
 import { IToolchainNpmOptions } from './schema';
+import debugLib from 'debug';
+
+const debug = debugLib('@wyntau/schematics:toolchain-npm');
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -20,6 +23,13 @@ export function toolchainNpm(_options: IToolchainNpmOptions): Rule {
   const options = camelCasedOptions(_options, 'toolchain-npm');
 
   return chain([
+    function (tree: Tree) {
+      if (!tree.exists('/package.json')) {
+        debug('create package.json');
+        tree.create('package.json', '{}');
+      }
+      return tree;
+    },
     mergeWith(
       apply(url('./files'), [
         contentTemplate({ registry: options.withRegistry, engineStrict: options.enableEngineStrict }),
@@ -29,7 +39,9 @@ export function toolchainNpm(_options: IToolchainNpmOptions): Rule {
     options.enableNvm ? schematic('toolchain-nvm', _options) : noop(),
     function (tree: Tree, context: SchematicContext) {
       context.addTask(
-        new NodePackageInstallTask({ packageManager: options.enableYarn ? 'yarn' : 'npm', hideOutput: false })
+        new NodePackageInstallTask({
+          packageManager: options.enableYarn ? 'yarn' : 'npm',
+        })
       );
       return tree;
     },
