66e441a3da18a5fca22a61527e52fd6350e865ce
wyntau <github@wtliu.com>
2021-10-16 14:50:45 +0800

    fix(starter-typescript): fix schematics order

--- src/starter-typescript/index.ts
+++ src/starter-typescript/index.ts
@@ -14,30 +14,34 @@ export function starterTypescript(_options: IStarterTypescriptOptions): Rule {
   return chain([
     schematic('toolchain-npm', {}),
     schematic('toolchain-typescript', {}),
-    options.toolchainCommitlint
-      ? schematic('toolchain-commitlint', unprefixedOptions(rawOptions, 'toolchain-commitlint'))
-      : noop(),
+
+    options.toolchainNvm ? schematic('toolchain-nvm', unprefixedOptions(rawOptions, 'toolchain-nvm')) : noop(),
+    options.toolchainYarn ? schematic('toolchain-yarn', unprefixedOptions(rawOptions, 'toolchain-yarn')) : noop(),
+
     options.toolchainEslint
       ? schematic(
           'toolchain-eslint',
           Object.assign({ target: _options.target }, unprefixedOptions(rawOptions, 'toolchain-eslint'))
         )
       : noop(),
+    options.toolchainPrettier
+      ? schematic('toolchain-prettier', unprefixedOptions(rawOptions, 'toolchain-prettier'))
+      : noop(),
+
     options.toolchainHusky ? schematic('toolchain-husky', unprefixedOptions(rawOptions, 'toolchain-husky')) : noop(),
-    options.toolchainLerna ? schematic('toolchain-lerna', unprefixedOptions(rawOptions, 'toolchain-lerna')) : noop(),
-    options.toolchainLintRecently
-      ? schematic('toolchain-lint-recently', unprefixedOptions(rawOptions, 'toolchain-lint-recently'))
+    options.toolchainCommitlint
+      ? schematic('toolchain-commitlint', unprefixedOptions(rawOptions, 'toolchain-commitlint'))
       : noop(),
     options.toolchainLintStaged
       ? schematic('toolchain-lint-staged', unprefixedOptions(rawOptions, 'toolchain-lint-staged'))
       : noop(),
-    options.toolchainNvm ? schematic('toolchain-nvm', unprefixedOptions(rawOptions, 'toolchain-nvm')) : noop(),
+
+    options.toolchainLerna ? schematic('toolchain-lerna', unprefixedOptions(rawOptions, 'toolchain-lerna')) : noop(),
+    options.toolchainLintRecently
+      ? schematic('toolchain-lint-recently', unprefixedOptions(rawOptions, 'toolchain-lint-recently'))
+      : noop(),
     options.toolchainPatchPackage
       ? schematic('toolchain-patch-package', unprefixedOptions(rawOptions, 'toolchain-patch-package'))
       : noop(),
-    options.toolchainPrettier
-      ? schematic('toolchain-prettier', unprefixedOptions(rawOptions, 'toolchain-prettier'))
-      : noop(),
-    options.toolchainYarn ? schematic('toolchain-yarn', unprefixedOptions(rawOptions, 'toolchain-yarn')) : noop(),
   ]);
 }
