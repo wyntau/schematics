0f937047aec15de402a1c76de509f47287643375
wyntau <github@wtliu.com>
2021-10-17 21:28:07 +0800

    refactor(shared/utility/sharedOptions): normalize

--- src/shared/utility/sharedOptions.ts
+++ src/shared/utility/sharedOptions.ts
@@ -1,11 +1,21 @@
 import { schematics } from '../../collection.json';
+import debugLib from 'debug';
 
+const debug = debugLib('@wyntau/schematics:shared/utility/sharedOptions');
 const optionsMap: Partial<Record<keyof typeof schematics, Record<string, any>>> = {};
 
-export function sharedOptionsOf<T extends Record<string, any>>(schematicName: keyof typeof schematics): T {
-  return (optionsMap[schematicName] || {}) as any;
+type SchematicName = keyof typeof schematics;
+
+export function sharedOptionsOf<T extends Record<string, any>>(
+  schematicName: SchematicName,
+  requestFrom: SchematicName
+): T {
+  const sharedOptions = (optionsMap[schematicName] || {}) as any;
+  debug('%s get sharedOptions of %s: %O', requestFrom, schematicName, sharedOptions);
+  return sharedOptions;
 }
 
-export function shareOptions(schematicName: keyof typeof schematics, options: Record<string, any>) {
+export function shareOptions(schematicName: SchematicName, options: Record<string, any>) {
+  debug('%s shareOptions: %O', schematicName, options);
   return (optionsMap[schematicName] = options);
 }
