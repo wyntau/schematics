d4dd5c36b6ea208363464b20cd2648134fd711bc
wyntau <github@wtliu.com>
2021-10-20 10:26:20 +0800

    refactor: use packageJson.addPackageJsonScript

--- src/shared/rules/packageJson.ts
+++ src/shared/rules/packageJson.ts
@@ -3,19 +3,20 @@ import {
   addPackageJsonDependency as _addPackageJsonDependency,
   NodeDependencyType,
 } from '@schematics/angular/utility/dependencies';
+import { JSONFile } from '@schematics/angular/utility/json-file';
 import { cyan, redBright } from 'colorette';
 import { debugLib } from '../utility/debug';
 
 export { NodeDependencyType } from '@schematics/angular/utility/dependencies';
 
-const debug = debugLib('shared/rules/dependencies');
+const debug = debugLib('shared/rules/packageJson');
 
 export function addPackageJsonDependency<T extends Record<string, string>>(
   latestVersions: T,
   packageNames: Array<keyof T> = Object.keys(latestVersions),
   type: NodeDependencyType = NodeDependencyType.Default
 ): Rule {
-  return function (tree: Tree): Tree {
+  return function (tree: Tree) {
     packageNames.forEach((_packageName) => {
       const packageName = _packageName as string;
       const packageVersion = latestVersions[packageName];
@@ -31,6 +32,17 @@ export function addPackageJsonDependency<T extends Record<string, string>>(
         name: packageName,
         version: packageVersion,
       });
+    });
+    return tree;
+  };
+}
+
+export function addPackageJsonScript(scripts: Record<string, string>): Rule {
+  return function (tree: Tree): Tree {
+    const packageJson = new JSONFile(tree, 'package.json');
+    Object.keys(scripts).forEach((name) => {
+      packageJson.modify(['scripts', name], scripts[name]);
+      debug('add npm script %s: %s', name, scripts[name]);
     });
     return tree;
   };
