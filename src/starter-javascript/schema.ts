443e6406f5bb6fbcd05d7859e2a52ef43a556789
wyntau <wyntau@outlook.com>
2021-10-20 09:38:26 +0800

    feat(starter-javascript): add renovate choose

--- src/starter-javascript/schema.ts
+++ src/starter-javascript/schema.ts
@@ -2,7 +2,15 @@ export { default as schemaJson } from './schema.json';
 export interface IStarterJavascriptOptions {
   'toolchain-typescript': boolean;
   toolchain: Array<
-    'commitlint' | 'eslint' | 'husky' | 'lerna' | 'lint-recently' | 'lint-staged' | 'patch-package' | 'prettier'
+    | 'commitlint'
+    | 'eslint'
+    | 'husky'
+    | 'lerna'
+    | 'lint-recently'
+    | 'lint-staged'
+    | 'patch-package'
+    | 'prettier'
+    | 'renovate'
   >;
   target: 'node' | 'react' | 'react-enable-jsx-runtime' | 'vue';
 }
