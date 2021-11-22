b733c9a8a0a2df44e4367b176383a1016faeb089
wyntau <github@wtliu.com>
2021-11-22 19:21:40 +0800

    refactor(toolchain-lernam): change useWorkspaces when enableYarn is false

--- src/toolchain-lerna/index.ts
+++ src/toolchain-lerna/index.ts
@@ -1,4 +1,4 @@
-import { apply, chain, contentTemplate, Rule, url } from '@angular-devkit/schematics';
+import { apply, chain, contentTemplate, Rule, Tree, url } from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependencyType } from '../shared/rules/packageJson';
 import { mergeWithIfNotExist } from '../shared/rules/files';
 import { camelCasedOptions } from '../shared/schema';
@@ -7,6 +7,7 @@ import { dependencies } from './latest-versions/package.json';
 import { sharedOptionsOf } from '../shared/utility/sharedOptions';
 import { IToolchainNpmOptions } from '../toolchain-npm/schema';
 import { CamelCasedProperties } from 'type-fest';
+import { JSONFile } from '@schematics/angular/utility/json-file';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -23,5 +24,15 @@ export function toolchainLerna(_options: IToolchainLernaOptions): Rule {
         contentTemplate({ npmClient: options.enableYarn || toolchainNpmOptions.enableYarn ? 'yarn' : 'npm' }),
       ])
     ),
+    function (tree: Tree) {
+      if (!options.enableYarn && !toolchainNpmOptions.enableYarn) {
+        return tree;
+      }
+
+      const lernaJson = new JSONFile(tree, 'lerna.json');
+      lernaJson.modify(['useWorkspaces'], true);
+      lernaJson.modify(['packages'], undefined);
+      return tree;
+    },
   ]);
 }
